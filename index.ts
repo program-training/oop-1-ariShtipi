c9acbe847fa8e20bf3ac240d9d523234deec47ef
ari shtipi <arish186777@gmail.com>
2023-08-27 11:37:57 +0300

    section 1  Rectangle

--- index.ts
+++ index.ts
@@ -1,11 +1,70 @@
-// את הקוד שלכם תכתבו כאן
+// class Rectangle {
+//   hight: number;
+//   width: number;
 
-//  1. Rectangle - מלבן
+//   constructor(hight: number, width: number) {
+//     this.hight = hight;
+//     this.width = width;
+//   }
+//   area(): number {
+//     return this.hight * this.width;
+//   }
+// }
 
-//  2. Square - ריבוע
+class Square {
+  rib: number;
+  constructor(rib: number) {
+    this.rib = rib;
+  }
+  area(): number {
+    return this.rib * this.rib;
+  }
+}
 
-// 3. Shape - צורה
+class Shape {
+  rib: number;
+  constructor(rib: number) {
+    this.rib = rib;
+  }
+  info(): void {
+    console.log("This is a Shape");
+  }
+}
 
+class Rectangle {
+  hight: number;
+  width: number;
+  constructor(hight: number, width: number) {
+    this.hight = hight;
+    this.width = width;
+  }
+  area(): number {
+    return this.hight * this.width;
+  }
+  info(): void {
+    console.log("This is a Rectangle");
+  }
+  scale(number): number {
+    return this.hight+number * this.width+number;
+  }
+  static anotherStaticMethod(r1: Rectangle, r2: Rectangle): Rectangle {
+    const combinedArea = r1.area() + r2.area();
+    return new Rectangle(combinedArea, 1); 
+  }
+
+}
+
+class ColoredRectangle extends Rectangle {
+  color: string;
+  constructor(hight: number, width: number, color: string) {
+    super(hight, width);
+    this.color = color;
+  }
+  info(): void {
+    console.log(`This is a Rectangle in color ${this.color}`);
+  }
+}
 // 4. Method Chaining
 
 // 5. Shape with draw method
+
