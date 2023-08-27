14cf3d877d4135545a3f2ef49cdcd9e5e638049b
ari shtipi <arish186777@gmail.com>
2023-08-27 13:38:44 +0300

    section 5 fixed'

--- index.ts
+++ index.ts
@@ -20,84 +20,84 @@
 //   }
 // }
 
-class Shape {
-  info(): void {
-    console.log("This is a Shape");
-  }
-}
-
-class Rectangle extends Shape {
-  hight: number;
-  width: number;
-  constructor(hight: number, width: number) {
-    super();
-    this.hight = hight;
-    this.width = width;
-  }
-  area(): number {
-    return this.hight * this.width;
-  }
-  info(): void {
-    console.log("This is a Rectangle");
-  }
-  scale(num:number): this {
-    this.hight *= num;
-    this.width *= num;
-    return this;
-  }
-  static anotherStaticMethod(r1: Rectangle, r2: Rectangle): Rectangle {
-    return new Rectangle(r1.width + r2.width, r1.hight + r2.hight);
-  }
-}
-class ColoredRectangle extends Rectangle {
-  color: string;
-  constructor(hight: number, width: number, color: string) {
-    super(hight, width);
-    this.color = color;
-  }
-  info(): void {
-    console.log(`This is a Rectangle in color ${this.color}`);
-  }
-}
-
 // class Shape {
-//   draw(): void {
-//     console.log("drawing a shape");
+//   info(): void {
+//     console.log("This is a Shape");
 //   }
 // }
 
-// class Circle extends Shape {
-//   draw(): void {
-//     console.log("drawing a circle");
+// class Rectangle extends Shape {
+//   hight: number;
+//   width: number;
+//   constructor(hight: number, width: number) {
+//     super();
+//     this.hight = hight;
+//     this.width = width;
+//   }
+//   area(): number {
+//     return this.hight * this.width;
+//   }
+//   info(): void {
+//     console.log("This is a Rectangle");
+//   }
+//   scale(num:number): this {
+//     this.hight *= num;
+//     this.width *= num;
+//     return this;
+//   }
+//   static anotherStaticMethod(r1: Rectangle, r2: Rectangle): Rectangle {
+//     return new Rectangle(r1.width + r2.width, r1.hight + r2.hight);
+//   }
+// }
+// class ColoredRectangle extends Rectangle {
+//   color: string;
+//   constructor(hight: number, width: number, color: string) {
+//     super(hight, width);
+//     this.color = color;
+//   }
+//   info(): void {
+//     console.log(`This is a Rectangle in color ${this.color}`);
 //   }
 // }
 
-// class Triangle extends Shape {
-//   draw(): void {
-//     console.log("drawing a triangle");
-//   }
-// }
+class Shape {
+  draw(): void {
+    console.log("drawing a shape"); 
+  }
+}
 
-// class Square extends Shape {
-//   draw(): void {
-//     console.log("drawing a square");
-//   }
-// }
+class Circle extends Shape {
+  draw(): void {
+    console.log("drawing a circle");
+  }
+}
 
-// function renderShapes(shapes: Shape[]): void {
-//   for (const shape of shapes) {
-//     shape.draw();
-//   }
-// }
-// const shapes: Shape[] = [
-//   new Shape(),
-//   new Circle(),
-//   new Triangle(),
-//   new Square(),
-// ];
+class Triangle extends Shape {
+  draw(): void {
+    console.log("drawing a triangle");
+  }
+}
 
-// renderShapes(shapes);
+class Square extends Shape {
+  draw(): void {
+    console.log("drawing a square");
+  }
+}
 
-const rect = new Rectangle(5, 10);
-rect.scale(2).info();
-console.log(rect.area());
+function renderShapes(shapes: Shape[]): void {
+  for (const shape of shapes) {
+    shape.draw();
+  }
+}
+const shapes: Shape[] = [
+  new Shape(),
+  new Circle(),
+  new Triangle(),
+  new Square(),
+];
+
+renderShapes(shapes);
+
+// const rect = new Rectangle(5, 10);
+// console.log(rect.scale(2));
+
