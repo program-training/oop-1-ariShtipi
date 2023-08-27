0e4810150e7d71287bae427037a1c0195b732de5
ari shtipi <arish186777@gmail.com>
2023-08-27 11:54:42 +0300

    section 5 Shape with Draw Method

--- index.ts
+++ index.ts
@@ -11,60 +11,86 @@
 //   }
 // }
 
-class Square {
-  rib: number;
-  constructor(rib: number) {
-    this.rib = rib;
-  }
-  area(): number {
-    return this.rib * this.rib;
-  }
-}
+// class Square extends Rectangle {
+//   constructor(width: number) {
+//     super(width, width);
+//   }
+//   area(): number {
+//     return this.width * this.width;
+//   }
+// }
+
+// class Shape {
+//   info(): void {
+//     console.log("This is a Shape");
+//   }
+// }
+
+// class Rectangle extends Shape {
+//   hight: number;
+//   width: number;
+//   constructor(hight: number, width: number) {
+//     super();
+//     this.hight = hight;
+//     this.width = width;
+//   }
+//   area(): number {
+//     return this.hight * this.width;
+//   }
+//   info(): void {
+//     console.log("This is a Rectangle");
+//   }
+//   scale(number): number {
+//     return this.hight * number * this.width * number;
+//   }
+//   static anotherStaticMethod(r1: Rectangle, r2: Rectangle): Rectangle {
+//     return new Rectangle(r1.width + r2.width, r1.hight + r2.hight);
+//   }
+// }
+// class ColoredRectangle extends Rectangle {
+//   color: string;
+//   constructor(hight: number, width: number, color: string) {
+//     super(hight, width);
+//     this.color = color;
+//   }
+//   info(): void {
+//     console.log(`This is a Rectangle in color ${this.color}`);
+//   }
+// }
 
 class Shape {
-  rib: number;
-  constructor(rib: number) {
-    this.rib = rib;
-  }
-  info(): void {
-    console.log("This is a Shape");
+  draw(): void {
+    console.log("drawing a shape");
   }
 }
 
-class Rectangle {
-  hight: number;
-  width: number;
-  constructor(hight: number, width: number) {
-    this.hight = hight;
-    this.width = width;
-  }
-  area(): number {
-    return this.hight * this.width;
-  }
-  info(): void {
-    console.log("This is a Rectangle");
-  }
-  scale(number): number {
-    return this.hight+number * this.width+number;
-  }
-  static anotherStaticMethod(r1: Rectangle, r2: Rectangle): Rectangle {
-    const combinedArea = r1.area() + r2.area();
-    return new Rectangle(combinedArea, 1); 
-  }
-
-}
-
-class ColoredRectangle extends Rectangle {
-  color: string;
-  constructor(hight: number, width: number, color: string) {
-    super(hight, width);
-    this.color = color;
-  }
-  info(): void {
-    console.log(`This is a Rectangle in color ${this.color}`);
+class Circle extends Shape {
+  draw(): void {
+    console.log("drawing a circle");
   }
 }
-// 4. Method Chaining
 
-// 5. Shape with draw method
+class Triangle extends Shape {
+  draw(): void {
+    console.log("drawing a triangle");
+  }
+}
 
+class Square extends Shape {
+  draw(): void {
+    console.log("drawing a square");
+  }
+}
+
+function renderShapes(shapes: Shape[]): void {
+  for (const shape of shapes) {
+    shape.draw();
+  }
+}
+const shapes: Shape[] = [
+  new Shape(),
+  new Circle(),
+  new Triangle(),
+  new Square(),
+];
+renderShapes(shapes);
